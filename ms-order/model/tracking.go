8368335b2ae6f2a347348f83d637bd8f319ead63
kiet <kiet123pascal@gmail.com>
2024-02-06 22:38:20 +0700

    add tracking

--- ms-order/model/tracking.go
+++ ms-order/model/tracking.go
@@ -33,3 +33,14 @@ type History struct {
 	Date        string `json:"date"`
 	Description string `json:"desc"`
 }
+
+type Courier struct {
+	AWB         string    `json:"awb"`
+	Company     string    `json:"company"`
+	Status      string    `json:"status"`
+	Date        string    `json:"date"`
+	Fee         float64   `json:"fee"`
+	Origin      string    `json:"origin"`
+	Destination string    `json:"destination"`
+	History     []History `json:"history"`
+}
